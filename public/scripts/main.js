84699306ed28f843cbff30eaf0a4ff5b27193868
Saad <isaadibrahim2005@gmail.com>
2024-05-14 23:49:41 +0500

    Footer added, major ui improvements

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,14 @@
 console.log("Script Running!");
 const learn = document.getElementById("learnmore");
 const navi = document.getElementById("navi");
+const mainBody = document.getElementById('main-content');
+const hideCont = document.getElementById('hide-cont');
+const foot = document.getElementById('foot');
 
 learn.addEventListener("click", ()=>{
     console.log("Learn More Clicked!");
     // document.querySelector('nav').classList.add('newStyle');
     navi.classList.add("newStyle"); 
+    mainBody.style.display = "block";
+    hideCont.style.display = "none";
 });
