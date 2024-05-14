ebbf2c00811edf9e4c8fe763f5e839bfa5cc663f
Saad <isaadibrahim2005@gmail.com>
2024-05-14 22:48:59 +0500

    Nav bar ig

---
+++ public/scripts/main.js
@@ -0,0 +1,6 @@
+console.log("Script Running!");
+const learn = document.getElementById("learnmore");
+
+learn.addEventListener("click", ()=>{
+    document.querySelector('nav').classList.add('newStyle');
+});
