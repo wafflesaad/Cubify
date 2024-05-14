1c67ec972c0b2ed6d2f488fc47bf96200ab1f3f8
Saad <isaadibrahim2005@gmail.com>
2024-05-14 21:46:57 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
